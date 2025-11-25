71508962a5e4e4a7e458739f1061881fe9485489
Piyush Babhulakar <65109923+Piyushbabhulakar12@users.noreply.github.com>
2025-11-25 14:34:33 +0530

    ..

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,18 @@
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
-const path = require("path");
+// const PrerenderSPAPlugin = require("prerender-spa-plugin");
+// const path = require("path");
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
-module.exports = function override(config, env) {
-  if (env === "production") {
-    config.plugins.push(
-      new PrerenderSPAPlugin({
-        staticDir: config.output.path,
-        routes: ["/"],
-        minify: {
-          collapseWhitespace: true,
-          keepClosingSlash: true,
-        },
-        renderer: new PrerenderSPAPlugin.PuppeteerRenderer({
-          headless: true,
-          renderAfterTime: 1000, // short wait to avoid timeout
-        }),
-        postProcess(renderedRoute) {
-          // ignore prerender failures to let build succeed
-          return renderedRoute;
-        },
-      })
-    );
-  }
-  return config;
-};
+// module.exports = function override(config, env) {
+//   if (env === "production") {
+//     config.plugins.push(
+//       new PrerenderSPAPlugin({
+//         staticDir: path.join(__dirname, "build"),
+//         routes: ["/"], // make sure your routes exist
+//         renderer: new Renderer({
+//           renderAfterDocumentEvent: "render-event", // see note below
+//         }),
+//       })
+//     );
+//   }
+//   return config;
+// };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter, StaticRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-// Detect if we are prerendering (running in Node/Puppeteer)
-const isPrerendering = typeof window === "undefined";
-const Router = isPrerendering ? StaticRouter : BrowserRouter;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router location={isPrerendering ? "/" : undefined}>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </Router>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
